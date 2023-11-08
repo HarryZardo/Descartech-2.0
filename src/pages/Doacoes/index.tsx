4e898f6cd672ec04335e56f27a79ea684811296f
MelChechiS <128297617+MelChechiS@users.noreply.github.com>
2023-11-08 15:04:03 -0300

    commit

--- src/pages/Doacoes/index.tsx
+++ src/pages/Doacoes/index.tsx
@@ -1,50 +1,52 @@
 import styles from "./Doacoes.module.scss";
 import Toolbar from "../../components/Toolbar";
 import Footer from "../../components/Footer";
-import Mel from "../../assets/desenvolvedores/mel.webp"
-
+import Billie from "../../assets/usuários/billie.png";
 
 export default function Doacoes() {
     return (
-        <div>
-            <Toolbar />
-            <body>
-                <div>
-                    <div className={styles.container}>
-                        <div className={styles.informacion}>
-                            <img className={styles.doador} src={Mel} alt="doador eletrônico"></img>
-                            <h3>Melany Chechi Silverio</h3>
-                        <div>
-                    <div>
-                      <p> Mouse</p>
-                      <p> Teclado</p>
-                      <p> Fone de Ouvido</p>
-                    </div>
-                    <br></br>
-                    <button className={styles.Button}>Abrir</button>
-                    </div>
-                    </div>
-                    </div>
-                </div>
+        <body>
+<header>
+    <Toolbar />
+</header>
 
-            <div>
-                <div className={styles.container}>
-                    <div className={styles.informacion}>
-                        <img className={styles.doador} src={Mel} alt="doador eletrônico"></img>
-                        <h3>Melany Chechi Silverio</h3>
-                    <div>
-                      <p> Mouse</p>
-                      <p> Teclado</p>
-                      <p> Fone de Ouvido</p>
-                      <br></br>
-                      <button className={styles.Button}>Abrir</button>
-                    </div>
-                </div>
-             </div>
-            </div>
-           
-        </body>
-            <Footer />
+        <div className={styles.cardContainer}>
+      <div className={styles.card}>
+        <img className={styles.img} src={Billie} alt="Foto da Pessoa" />
+        <div className={styles.info}>
+          <h2>Billie Eilish</h2>
+          <div>
+            <ul>
+              <li>Bolinha 1</li>
+              <li>Bolinha 2</li>
+              <li>Bolinha 3</li>
+            </ul>
+          </div>
         </div>
-    );
+        <button className={styles.button}>Abrir</button>
+      </div>
+
+      {/* Segundo card */}
+      <div className={styles.card}>
+        <img className={styles.img} src={Billie} alt="Foto da Pessoa" />
+        <div className={styles.info}>
+          <h2>Lero Lero</h2>
+          <div>
+
+            <ul>
+              <li>Bolinha 4</li>
+              <li>Bolinha 5</li>
+              <li>Bolinha 6</li>
+            </ul>
+          </div>
+        </div>
+        <button className={styles.button}>Abrir</button>
+      </div>
+    </div>
+
+    <div>
+        <Footer />
+    </div>
+    </body>
+  );
 }
