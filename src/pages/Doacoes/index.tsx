c32e019b80fed9164307ed12a10f7f98eaeec396
MelChechiS <128297617+MelChechiS@users.noreply.github.com>
2023-11-09 09:31:17 -0300

    Page "Doações"

--- src/pages/Doacoes/index.tsx
+++ src/pages/Doacoes/index.tsx
@@ -1,52 +1,65 @@
 import styles from "./Doacoes.module.scss";
 import Toolbar from "../../components/Toolbar";
 import Footer from "../../components/Footer";
-import Billie from "../../assets/usuários/billie.png";
+import user1 from "../../assets/usuários/user1.png";
+import user2 from "../../assets/usuários/user2.png";
+import user3 from "../../assets/usuários/user3.png";
 
 export default function Doacoes() {
     return (
-        <body>
-<header>
-    <Toolbar />
-</header>
-
-        <div className={styles.cardContainer}>
-      <div className={styles.card}>
-        <img className={styles.img} src={Billie} alt="Foto da Pessoa" />
-        <div className={styles.info}>
-          <h2>Billie Eilish</h2>
-          <div>
-            <ul>
-              <li>Bolinha 1</li>
-              <li>Bolinha 2</li>
-              <li>Bolinha 3</li>
-            </ul>
-          </div>
-        </div>
-        <button className={styles.button}>Abrir</button>
-      </div>
-
-      {/* Segundo card */}
-      <div className={styles.card}>
-        <img className={styles.img} src={Billie} alt="Foto da Pessoa" />
-        <div className={styles.info}>
-          <h2>Lero Lero</h2>
-          <div>
-
-            <ul>
-              <li>Bolinha 4</li>
-              <li>Bolinha 5</li>
-              <li>Bolinha 6</li>
-            </ul>
-          </div>
-        </div>
-        <button className={styles.button}>Abrir</button>
-      </div>
-    </div>
-
+      <body>
+    <header>
+      <Toolbar />
+    </header>
     <div>
-        <Footer />
+
+    <h1 className={styles.doacoes_title}>Área de Doações</h1>
+
+    <div className={styles.card_container}>
+<div className={styles.user_card}>
+    <img className={styles.img_user} src={user1} alt="Leandro"></img>
+    <div className={styles.user_info}>
+      <h2 className={styles.user_info__name}>Leandro Silva Gomes</h2>
+      <ul>
+        <li>Teclado</li>
+        <li>Mouse</li>
+        <li>Celular</li>
+      </ul>
     </div>
+    <button className={styles.abrir_button}>Abrir</button>
+  </div>
+
+  <div className={styles.user_card}>
+    <img className={styles.img_user} src={user2} alt="Ana"></img>
+    <div className={styles.user_info}>
+      <h2 className={styles.user_info__name}>Ana Mirellis Antônio</h2>
+      <ul>
+        <li>Monitor</li>
+        <li>Estabilizador</li>
+        <li>Notebook</li>
+      </ul>
+    </div>
+    <button className={styles.abrir_button}>Abrir</button>
+  </div>
+
+  <div className={styles.user_card}>
+    <img className={styles.img_user} src={user3} alt="Donisete"></img>
+    <div className={styles.user_info}>
+      <h2 className={styles.user_info__name}>Donisete Oliveira</h2>
+      <ul>
+        <li>Televisão</li>
+        <li>Geladeira</li>
+        <li>Cabo de Rede</li>
+      </ul>
+    </div>
+    <button className={styles.abrir_button}>Abrir</button>
+  </div>
+</div>
+    </div>
+
+  <div>
+    <Footer />
+  </div>
     </body>
   );
 }
